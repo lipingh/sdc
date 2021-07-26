0aa9fcec85512c8ffd46863f9a967841017d58b3
lipingh <lipingh@andrew.cmu.edu>
2021-07-26 16:54:19 -0700

    format date to Month dd, yyyy format

--- client/src/RatingsAndReviews/ReviewListItem.jsx
+++ client/src/RatingsAndReviews/ReviewListItem.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ReviewListItem = ({ review }) => (
-  <div>
+const ReviewListItem = ({ review }) => {
+  const formatDate = (dateString) => {
+    const d = new Date(dateString);
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December',
+    ];
+    const month = monthNames[d.getMonth()];
+    const day = d.getDate() + 1;
+    const year = d.getFullYear();
+    return `${month} ${day}, ${year}`;
+  };
+
+  return (
     <div>
-      <span>{review.rating}</span>
-      <span>{review.reviewer_name}</span>
-      <span>
-        ,
-        {review.date}
-      </span>
+      <div>
+        <span>{review.rating}</span>
+        <span>{review.reviewer_name}</span>
+        <span>
+          ,
+          {formatDate(review.date)}
+        </span>
+      </div>
+      <div>{review.body}</div>
+      <div>
+        {review.recommend ? 'I recommend this product' : ''}
+      </div>
+      {/* {review.response ? <div>{review.response}</div> : ''} */}
+      <div>
+        <span>
+          Helpful? Yes(
+          {review.helpfulness}
+          )
+        </span>
+        <span>
+          | Report(0)
+        </span>
+      </div>
+      <br />
     </div>
-    <div>{review.body}</div>
-    <div>
-      {review.recommend ? 'I recommend this product' : ''}
-    </div>
-    {/* {review.response ? <div>{review.response}</div> : ''} */}
-    <div>
-      <span>
-        Helpful? Yes(
-        {review.helpfulness}
-        )
-      </span>
-      <span>
-        | Report(0)
-      </span>
-    </div>
-    <br />
-  </div>
-);
+  );
+};
 ReviewListItem.propTypes = {
   review: PropTypes.shape({
     rating: PropTypes.number,
