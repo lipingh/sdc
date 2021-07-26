4bb1d286ccb8e3bd73e4ab931c3ecfa47191b905
rachel <jones.rachel.a464@gmail.com>
2021-07-26 12:40:38 -0700

    create new component files and add package-lock.json to gitignore

--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import QuestionsList from './QuestionsList.jsx';
 
 const QuestionsAndAnswers = () => (
   <div>
---
+++ client/src/QuestionsAndAnswers/QuestionsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const QuestionsList = () => (
+  <div>
+    <h2>hello from QuestionsList</h2>
+  </div>
+);
+
+export default QuestionsList;
