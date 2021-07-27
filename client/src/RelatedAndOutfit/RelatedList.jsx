8887e75d2b9fbb3c3bbb5976806a66e08ff1e59a
Nick Hanrahan <nicholashanr@gmail.com>
2021-07-26 21:28:23 -0700

    Prepare RelatedList Component for slider buttons

--- client/src/RelatedAndOutfit/RelatedList.jsx
+++ client/src/RelatedAndOutfit/RelatedList.jsx
@@ -4,15 +4,28 @@ import exampleData from './ExampleRelatedProducts.js';
 import './related-list.css';
 
 const RelatedList = () => {
-  const thing = '';
+  const [current, setCurrent] = useState(0);
+  const len = exampleData.exampleRelated.length;
+  const width = window.innerWidth;
+  console.log('width: ', width)
+
+  const nextCard = () => {
+    setCurrent(current === len - 1 ? current : current + 1);
+  };
+
+  const prevCard = () => {
+    setCurrent(current === 0 ? 0 : current - 1);
+  };
+
+  console.log('current: ', current);
   return (
     <div className="related-list">
       {exampleData.exampleRelated.map((product) => (
         <RelatedCard key={product.id} product={product} />
       ))}
       <div className="list-actions">
-        <button className="btn-related-left">prev</button>
-        <button className="btn-related-right">next</button>
+        <button className="btn-related-left" onClick={prevCard}>prev</button>
+        <button className="btn-related-right" onClick={nextCard}>next</button>
       </div>
     </div>
   );
