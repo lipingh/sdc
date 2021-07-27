033a61ebe92720414c0060bf6c0e2e64f369a236
Jiaqian Zhu <blairzhu19@gmail.com>
2021-07-26 23:38:25 -0700

    left button will not show for the first image, right button will be hidden when the last image is selected

--- client/src/Overview/Gallery/Gallery.jsx
+++ client/src/Overview/Gallery/Gallery.jsx
@@ -6,7 +6,17 @@ import options from '../../config/config.js';
 const Gallery = () => {
   const [styles, setStyles] = useState([]);
   const [images, setImages] = useState([]);
-  const [currImg, setCurrImg] = useState('');
+  const [currImgIndex, setCurrImgIndex] = useState(0);
+
+  const buttonClickHandler = (direction) => (
+    (event) => {
+      if (direction === 'next') {
+        setCurrImgIndex((prevIndex) => prevIndex + 1);
+      } else if (direction === 'prev') {
+        setCurrImgIndex((prevIndex) => prevIndex - 1);
+      }
+    }
+  );
 
   useEffect(() => {
     axios.get(`${options.url}products/20100/styles`, { headers: options.headers })
@@ -14,7 +24,6 @@ const Gallery = () => {
         const imgs = response.data.results[0].photos.map((photo) => photo.url);
         setImages(imgs);
         setStyles(response.data.results);
-        setCurrImg(imgs[0]);
       })
       .catch((err) => {
         console.log('styles data fetching err', err);
@@ -25,9 +34,9 @@ const Gallery = () => {
     <div className={style.gallery}>
       <div className={style.thumbnail}></div>
       <div className={style.mainImage}>
-        <button type="button" className={style.clickPrev}>&lt;</button>
-        <img src={currImg} alt="selected style" className={style.image} />
-        <button type="button" className={style.clickNext}>&gt;</button>
+        {currImgIndex !== 0 && <button type="button" className={style.clickPrev} onClick={buttonClickHandler('prev')}>&lt;</button>}
+        <img src={images[currImgIndex]} alt="selected style" className={style.image} />
+        {currImgIndex !== images.length - 1 && <button type="button" className={style.clickNext} onClick={buttonClickHandler('next')}>&gt;</button>}
       </div>
     </div>
   );
