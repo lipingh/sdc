437eed0742d13df1baab7db0e89f1faaa11d2b3f
Nick Hanrahan <nicholashanr@gmail.com>
2021-07-26 17:25:46 -0700

    Implement horizontal list of product cards for related items list

--- client/src/RelatedAndOutfit/RelatedList.jsx
+++ client/src/RelatedAndOutfit/RelatedList.jsx
@@ -1,18 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import RelatedCard from './RelatedCard.jsx';
 import exampleData from './ExampleRelatedProducts.js';
 import './related-list.css';
 
-const RelatedList = () => (
-  <div className="related-list">
-    {exampleData.exampleRelated.map((product) => (
-      <RelatedCard key={product.id} product={product} />
+const RelatedList = () => {
+  const thing = '';
+  return (
+    <div className="related-list">
+      {exampleData.exampleRelated.map((product) => (
+        <RelatedCard key={product.id} product={product} />
       ))}
-    <div className="list-actions">
-      <button className="btn-related-left">prev</button>
-      <button className="btn-related-right">next</button>
+      <div className="list-actions">
+        <button className="btn-related-left">prev</button>
+        <button className="btn-related-right">next</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RelatedList;
