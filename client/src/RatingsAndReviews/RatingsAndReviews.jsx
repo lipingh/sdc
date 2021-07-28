3af975a7ad252b3785d2e0af37b19c5eb5a45406
lipingh <lipingh@andrew.cmu.edu>
2021-07-28 09:35:31 -0700

    remove caculate ratings to helper.js for gloabal use

--- client/src/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/src/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import StarRating from './StarRating.jsx';
 import options from '../config/config';
@@ -6,29 +6,34 @@ import RatingsBreakDown from './RatingsBreakDown.jsx';
 import ProductBreakDown from './ProductBreakDown.jsx';
 import ReviewList from './ReviewList.jsx';
 import './ratings.css';
+import calculateRating from "../../helper.js";
 
 const RatingsAndReviews = () => {
+  const productId = 13023;
   const [recommended, setRecommended] = useState(0);
   const [notRecommended, setNotRecommended] = useState(0);
   const [ratings, setRatings] = useState({});
   const [characteristics, setCharacteristics] = useState({});
-  const oneStar = ratings['1'] ? parseInt(ratings['1'], 10) : 0;
-  const twoStar = ratings['2'] ? parseInt(ratings['2'], 10) : 0;
-  const threeStar = ratings['3'] ? parseInt(ratings['3'], 10) : 0;
-  const fourStar = ratings['4'] ? parseInt(ratings['4'], 10) : 0;
-  const fiveStar = ratings['5'] ? parseInt(ratings['5'], 10) : 0;
-  const totalReviews = oneStar + twoStar + threeStar + fourStar + fiveStar;
-  const totalScores = oneStar + twoStar * 2 + threeStar * 3 + fourStar * 4 + fiveStar * 5;
-  const averageRatings = (totalScores / totalReviews);
-  const productId = 13023;
-  const ratingsBreakDown = {
-    oneStar,
-    twoStar,
-    threeStar,
-    fourStar,
-    fiveStar,
-    totalReviews,
-  };
+  // const [ratingsBreakDown, setRatingsBreakDown] = useState({});
+  const ratingsBreakDown = useMemo(() => calculateRating(ratings), [ratings]);
+  // const oneStar = ratings['1'] ? parseInt(ratings['1'], 10) : 0;
+  // const twoStar = ratings['2'] ? parseInt(ratings['2'], 10) : 0;
+  // const threeStar = ratings['3'] ? parseInt(ratings['3'], 10) : 0;
+  // const fourStar = ratings['4'] ? parseInt(ratings['4'], 10) : 0;
+  // const fiveStar = ratings['5'] ? parseInt(ratings['5'], 10) : 0;
+  // const totalReviews = oneStar + twoStar + threeStar + fourStar + fiveStar;
+  // const totalScores = oneStar + twoStar * 2 + threeStar * 3 + fourStar * 4 + fiveStar * 5;
+  // const averageRatings = (totalScores / totalReviews);
+
+  // let ratingsBreakDown = {
+  //   oneStar: 0,
+  //   twoStar: 0,
+  //   threeStar: 0,
+  //   fourStar: 0,
+  //   fiveStar: 0,
+  //   averageRatings: 0,
+  //   totalReviews: 0,
+  // };
 
   const getReviewsMeta = () => {
     axios({
@@ -41,6 +46,7 @@ const RatingsAndReviews = () => {
         setRecommended(parseInt(res.data.recommended.true, 10));
         setNotRecommended(parseInt(res.data.recommended.false, 10));
         setCharacteristics(res.data.characteristics);
+        // setRatingsBreakDown(calculateRating(ratings));
       })
       .catch((err) => { throw err; });
   };
@@ -55,8 +61,8 @@ const RatingsAndReviews = () => {
       <div className="ratings-reviews">
         <div className="breakdown">
           <div className="overall-rating">
-            <span>{averageRatings.toFixed(1)}</span>
-            <StarRating rating={averageRatings} />
+            <span>{ratingsBreakDown.averageRatings.toFixed(1)}</span>
+            <StarRating rating={ratingsBreakDown.averageRatings} />
           </div>
           <br />
           <RatingsBreakDown ratings={ratingsBreakDown} />
@@ -69,7 +75,7 @@ const RatingsAndReviews = () => {
           <ProductBreakDown characteristics={characteristics} />
         </div>
         <div className="review-list">
-          <ReviewList totalReviews={totalReviews} productId={productId} />
+          <ReviewList totalReviews={ratingsBreakDown.totalReviews} productId={productId} />
         </div>
       </div>
     </div>
