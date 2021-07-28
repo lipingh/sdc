117ef3a68a28728b49d858f1785e5ffda2a0cc0c
lipingh <lipingh@andrew.cmu.edu>
2021-07-28 09:37:01 -0700

    clean ratings and reviews code

--- client/src/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/src/RatingsAndReviews/RatingsAndReviews.jsx
@@ -6,7 +6,7 @@ import RatingsBreakDown from './RatingsBreakDown.jsx';
 import ProductBreakDown from './ProductBreakDown.jsx';
 import ReviewList from './ReviewList.jsx';
 import './ratings.css';
-import calculateRating from "../../helper.js";
+import calculateRating from '../../helper.js';
 
 const RatingsAndReviews = () => {
   const productId = 13023;
@@ -14,27 +14,7 @@ const RatingsAndReviews = () => {
   const [notRecommended, setNotRecommended] = useState(0);
   const [ratings, setRatings] = useState({});
   const [characteristics, setCharacteristics] = useState({});
-  // const [ratingsBreakDown, setRatingsBreakDown] = useState({});
   const ratingsBreakDown = useMemo(() => calculateRating(ratings), [ratings]);
-  // const oneStar = ratings['1'] ? parseInt(ratings['1'], 10) : 0;
-  // const twoStar = ratings['2'] ? parseInt(ratings['2'], 10) : 0;
-  // const threeStar = ratings['3'] ? parseInt(ratings['3'], 10) : 0;
-  // const fourStar = ratings['4'] ? parseInt(ratings['4'], 10) : 0;
-  // const fiveStar = ratings['5'] ? parseInt(ratings['5'], 10) : 0;
-  // const totalReviews = oneStar + twoStar + threeStar + fourStar + fiveStar;
-  // const totalScores = oneStar + twoStar * 2 + threeStar * 3 + fourStar * 4 + fiveStar * 5;
-  // const averageRatings = (totalScores / totalReviews);
-
-  // let ratingsBreakDown = {
-  //   oneStar: 0,
-  //   twoStar: 0,
-  //   threeStar: 0,
-  //   fourStar: 0,
-  //   fiveStar: 0,
-  //   averageRatings: 0,
-  //   totalReviews: 0,
-  // };
-
   const getReviewsMeta = () => {
     axios({
       url: `${options.url}reviews/meta?product_id=${productId}`,
@@ -46,7 +26,6 @@ const RatingsAndReviews = () => {
         setRecommended(parseInt(res.data.recommended.true, 10));
         setNotRecommended(parseInt(res.data.recommended.false, 10));
         setCharacteristics(res.data.characteristics);
-        // setRatingsBreakDown(calculateRating(ratings));
       })
       .catch((err) => { throw err; });
   };
