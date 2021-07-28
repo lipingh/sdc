b4525af9b76d09c06412647fa25b8e8e79162941
Nick Hanrahan <nicholashanr@gmail.com>
2021-07-28 15:51:13 -0700

    modal window finds shared features on render

--- client/src/RelatedAndOutfit/ComparisonModal.jsx
+++ client/src/RelatedAndOutfit/ComparisonModal.jsx
@@ -2,21 +2,43 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './comp-modal.css'
 
-const ComparisonModal = ({open, onClose, currProduct}) => {
+const ComparisonModal = ({open, onClose, product, currProduct}) => {
 
   if (!open) {
     return null;
   }
 
+  const sharedFeatures = currProduct.features.map((featureObj) => {
+    const featureName = featureObj.feature;
+    let sharedFeatureObj;
+    product.features.forEach((relatedFeatureObj) => {
+      if (relatedFeatureObj.feature === featureName) {
+        sharedFeatureObj = {name: featureName, currVal: featureObj.value, relatedVal: relatedFeatureObj.value};
+      }
+    })
+    return sharedFeatureObj;
+  })
+
   return ReactDOM.createPortal(
     <>
       <div className="comparison-overlay"/>
       <div className="comparison-window">
+        <div className="comparing">COMPARING</div>
         <button className="btn-close-comp-modal" onClick={onClose}>X</button>
         <div className="comp-table">
-          <div className="comp-current"></div>
-          <div className="comp-titles"></div>
-          <div className="comp-related"></div>
+          <div className="comp-titles">
+            <div className="comp-title">{product.name}</div>
+            <div className="comp-title">Features</div>
+            <div className="comp-title">{currProduct.name}</div>
+          </div>
+          <div className="comp-table-features">
+            <div className="comp-current">
+            </div>
+            <div className="comp-features">
+            </div>
+            <div className="comp-related">
+            </div>
+          </div>
         </div>
       </div>
     </>,
