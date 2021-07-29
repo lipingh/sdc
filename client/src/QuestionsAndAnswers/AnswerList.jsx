a1d52ba5653236d591bf97f4177d180e35cbf5a8
rachel <jones.rachel.a464@gmail.com>
2021-07-28 17:23:32 -0700

    changed to css for scroll

--- client/src/QuestionsAndAnswers/AnswerList.jsx
+++ client/src/QuestionsAndAnswers/AnswerList.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Collapsible from 'react-collapsible';
 import EachAnswer from './EachAnswer.jsx';
 
 const AnswerList = ({ answers }) => (
-  <ul>
-    <Collapsible trigger="show Answers" overflowWhenOpen="scroll" transitionTime="200">
-      {Object.keys(answers).map((answer) => (
-        <div key={answer}>
-          <EachAnswer key={answer} answer={answers[answer]} />
-        </div>
-      ))}
-    </Collapsible>
+  <ul className="answer-list">
+    {Object.keys(answers).map((answer) => (
+      <div key={answer}>
+        <EachAnswer key={answer} answer={answers[answer]} />
+      </div>
+    ))}
   </ul>
 );
 // for some reason .map will not work with answers.map now that answers is an array
--- client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './questions.css';
+import './QandA-list.css';
 import options from '../config/config.js';
 import QuestionsList from './QuestionsList.jsx';
 import allInOrder from './Helpers.js';
