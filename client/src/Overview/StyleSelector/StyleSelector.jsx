ebc55cc94d0f34e8aef2bbd9bfb7fbbd4e209dfd
Jiaqian Zhu <blairzhu19@gmail.com>
2021-07-29 10:57:30 -0700

    got style name displayed

--- client/src/Overview/StyleSelector/StyleSelector.jsx
+++ client/src/Overview/StyleSelector/StyleSelector.jsx
@@ -2,9 +2,31 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import options from '../../config/config.js';
 import { ExpandContext } from '../Overview.jsx';
+import style from './StyleSelector.module.css';
 
 const StyleSelector = () => {
-  return(<div>hello</div>)
+  const contextData = useContext(ExpandContext);
+  const [styleName, setStyleName] = useState('');
+  const [styleThumbnails, setStyleThumbnails] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${options.url}products/${contextData.currState.productId}/styles`, { headers: options.headers })
+      .then((response) => {
+        setStyleName(response.data.results[contextData.currState.styleIndex].name);
+      })
+      .catch((err) => {
+        console.log('styles data fetching err', err);
+      });
+  }, []);
+  return (
+    <div>
+      <>
+        <span className={style.styleText}>STYLE &gt;</span>
+        <span className={style.styleName}>{styleName}</span>
+      </>
+
+    </div>
+  );
 };
 
 export default StyleSelector;
