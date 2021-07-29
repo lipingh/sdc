6e3a2a43749616938ebf9abc418abf8dc384100a
Jiaqian Zhu <blairzhu19@gmail.com>
2021-07-28 17:21:46 -0700

    work on the review rating

--- client/src/Overview/ProductInfo/ProductInfo.jsx
+++ client/src/Overview/ProductInfo/ProductInfo.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import getReviewsMeta from '../../../reviewRequest.js';
+import { ExpandContext } from '../Overview.jsx';
+import calculateRating from '../../../helper.js';
+import StarRating from '../../RatingsAndReviews/StarRating.jsx'
 
-const ProductInfo = () => (
-  <div>
-    heelo from info
-  </div>
-);
+const ProductInfo = () => {
+  const contextData = useContext(ExpandContext);
+  const [averageRating, setAverageRating] = useState(0);
+
+  useEffect(() => {
+    getReviewsMeta(contextData.currState.productId)
+      .then((res) => {
+        setAverageRating(calculateRating(res.ratings).averageRatings);
+      })
+      .catch((err) => {
+        console.log('review star data fetching error', err);
+      });
+  }, []);
+
+  return (
+    <div>
+      heelo from info
+      <StarRating rating={averageRating} />
+    </div>
+  );
+};
 
 export default ProductInfo;
