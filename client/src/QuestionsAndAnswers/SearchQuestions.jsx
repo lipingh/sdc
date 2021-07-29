38fd08f78a43b62352d04c8b041a25057b5e12e3
rachel <jones.rachel.a464@gmail.com>
2021-07-29 12:55:37 -0700

    change some css for the search bar and add an icon

--- client/src/QuestionsAndAnswers/SearchQuestions.jsx
+++ client/src/QuestionsAndAnswers/SearchQuestions.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
-const SearchQuestions = () => (
+const SearchQuestions = ({ handleSearch }) => (
   <>
     <form>
-      <input type="text" placeholder="Have a Question? Search for Answers ..." />
-      <button type="submit">Search</button>
+      <input
+        className="search-bar"
+        type="text"
+        onChange={(e) => { handleSearch(e); }}
+        placeholder="Have a Question? Search for Answers ..."
+      />
+      <FontAwesomeIcon icon={faSearch} />
     </form>
   </>
 );
