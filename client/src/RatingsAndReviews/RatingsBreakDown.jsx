a6307b397e5291d85f1acfebe42f0756922344b0
lipingh <lipingh@andrew.cmu.edu>
2021-07-29 10:19:51 -0700

    rating breakdown css

--- client/src/RatingsAndReviews/RatingsBreakDown.jsx
+++ client/src/RatingsAndReviews/RatingsBreakDown.jsx
@@ -4,57 +4,57 @@ import PropTypes from 'prop-types';
 const RatingsBreakDown = ({ ratings, handleFilterByRating }) => (
   <div className="ratings-breakdown">
     <div
-      className="sidebar star5"
+      // className="sidebar star5"
       role="link"
       onClick={() => handleFilterByRating(5)}
       onKeyPress={() => { }}
       tabIndex={0}
     >
       <span className="star-label">5 stars:</span>
-      <span>{ratings.fiveStar}</span>
+      <span className="sidebar star5">{ratings.fiveStar}</span>
     </div>
     <div
-      className="sidebar star4"
+      // className="sidebar star4"
       role="link"
       onClick={() => handleFilterByRating(4)}
       onKeyPress={() => { }}
       tabIndex={0}
     >
       <span className="star-label">4 stars:</span>
-      <span>{ratings.fourStar}</span>
+      <span className="sidebar star4">{ratings.fourStar}</span>
     </div>
 
     <div
-      className="sidebar star3"
+      // className="sidebar star3"
       role="link"
       onClick={() => handleFilterByRating(3)}
       onKeyPress={() => { }}
       tabIndex={0}
     >
       <span className="star-label">3 stars:</span>
-      <span>{ratings.threeStar}</span>
+      <span className="sidebar star3">{ratings.threeStar}</span>
     </div>
 
     <div
-      className="sidebar star2"
+      // className="sidebar star2"
       role="link"
       onClick={() => handleFilterByRating(2)}
       onKeyPress={() => { }}
       tabIndex={0}
     >
       <span className="star-label">2 stars:</span>
-      <span>{ratings.twoStar}</span>
+      <span className="sidebar star2">{ratings.twoStar}</span>
     </div>
 
     <div
-      className="sidebar star1"
+      // className="sidebar star1"
       role="link"
       onClick={() => handleFilterByRating(1)}
       onKeyPress={() => { }}
       tabIndex={0}
     >
       <span className="star-label">1 stars:</span>
-      <span>{ratings.oneStar}</span>
+      <span className="sidebar star1">{ratings.oneStar}</span>
     </div>
   </div>
 
