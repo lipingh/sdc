dd0b36b4459e32effe71488e94649372e6b7d11e
Nick Hanrahan <nicholashanr@gmail.com>
2021-07-30 13:33:57 -0700

    Outfit list renders based on context state

--- client/src/RelatedAndOutfit/OutfitList.jsx
+++ client/src/RelatedAndOutfit/OutfitList.jsx
@@ -11,49 +11,16 @@ const OutfitList = () => {
   const [currProduct, setCurrProduct] = useState({});
   const [current, setCurrent] = useState(0);
   const [len, setLen] = useState(0);
-  const [outfits, setOutfits] = useState([]);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [cards, setCards] = useState(0);
   const outfitsContext = useContext(OutfitContext);
-
-  const getOutfitsFromIds = (idList) => {
-    const outfitsList = [];
-    idList.forEach((id) => {
-      axios.get(`${options.url}products/${id}`, {
-        headers: options.headers,
-      })
-        .then((res) => {
-          // condition should eventually use id of the current page (from global state) to ignore it
-          outfitsList.push(res.data);
-          const newOutfits = outfits.concat(outfitsList);
-          setOutfits(newOutfits);
-          setCurrProduct(res.data);
-        })
-        .catch((res, err) => {
-          res.end('Could not get outfits from ids: ', err);
-        });
-    });
-  };
-
-  // This should be refactored to get a list of ids from local storage
-  // const getOutfits = () => {
-  //   const storageOutfits = JSON.parse(window.localStorage.getItem('outfits'));
-  //   if (!Array.isArray(storageOutfits)) {
-  //     window.localStorage.setItem('outfits', JSON.stringify([13031]));
-  //     storageOutfits = [];
-  //   }
-  //   setLen(storageOutfits.length - 1);
-  //   getOutfitsFromIds(storageOutfits);
-  // };
 
   const updateWidth = () => {
     setWindowWidth(window.innerWidth);
   };
 
   useEffect(() => {
-    console.log('from component: ', outfitsContext.outfits);
     setLen(outfitsContext.outfits.length - 1);
-    getOutfitsFromIds(outfitsContext.outfits);
     window.addEventListener('resize', updateWidth);
     return () => { window.removeEventListener('resize', updateWidth); };
   }, []);
@@ -102,7 +69,7 @@ const OutfitList = () => {
     <div className="list-section">
       {current !== 0 && <button type="button" className="btn-list-left" onClick={prevCard}>prev</button>}
       <div className="list-cards" style={{ width: `${cards * 230}px` }} ref={listRef}>
-        {outfits.map((product) => (
+        {outfitsContext.outfits.map((product) => (
           <OutfitCard key={product.id} product={product} currProduct={currProduct} />
         ))}
       </div>
--- client/src/RelatedAndOutfit/RelatedAndOutfit.jsx
+++ client/src/RelatedAndOutfit/RelatedAndOutfit.jsx
@@ -1,30 +1,50 @@
 import React, { useState, useReducer, useEffect } from 'react';
+import axios from 'axios';
+import options from '../config/config.js';
 import RelatedList from './RelatedList.jsx';
 import OutfitList from './OutfitList.jsx';
 
 export const OutfitContext = React.createContext();
 
 const RelatedAndOutfit = () => {
+  const [outfits, setOutfits] = useState([]);
+
+  const getOutfitsFromIds = (idList) => {
+    const outfitsList = [];
+    idList.forEach((id) => {
+      axios.get(`${options.url}products/${id}`, {
+        headers: options.headers,
+      })
+        .then((res) => {
+          // condition should eventually use id of the current page (from global state) to ignore it
+          console.log('data: ', res.data);
+          outfitsList.push(res.data);
+          const newOutfits = outfits.concat(outfitsList);
+          setOutfits(newOutfits);
+        })
+        .catch((res, err) => {
+          res.end('Could not get outfits from ids: ', err);
+        });
+    });
+  };
+
   const getOutfits = () => {
     let storageOutfits = JSON.parse(window.localStorage.getItem('outfits'));
     if (!Array.isArray(storageOutfits)) {
       window.localStorage.setItem('outfits', JSON.stringify([13031]));
       storageOutfits = [];
     }
-    console.log('from parent: ', storageOutfits);
-    return storageOutfits;
+    getOutfitsFromIds(storageOutfits);
   };
 
-  const [outfits, setOutfits] = useState(getOutfits());
-
   useEffect(() => {
-    setOutfits(getOutfits());
+    getOutfits();
   }, []);
 
   return (
     <div id="comp-modal-portal" className="related-outfit">
       RELATED AND OUTFIT
-      <OutfitContext.Provider value={{ outfits, getOutfits }}>
+      <OutfitContext.Provider value={{ outfits, setOutfits }}>
         <div className="related-section">Related Products</div>
         <RelatedList />
         <div className="outfit-section">Your Outfit</div>
