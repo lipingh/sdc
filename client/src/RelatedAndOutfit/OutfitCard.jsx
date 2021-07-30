33260a402465b913e6ff3c8bf16a391bec07c04b
Nick Hanrahan <nicholashanr@gmail.com>
2021-07-29 19:17:41 -0700

    Copy RelatedList and RelatedCard file

---
+++ client/src/RelatedAndOutfit/OutfitCard.jsx
@@ -0,0 +1,132 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import options from '../config/config.js';
+import './related.css';
+import emptyStar from './assets/star-icon-empty.png';
+import fillStar from './assets/star-icon-fill.png';
+import ComparisonModal from './ComparisonModal.jsx';
+import StarRating from '../RatingsAndReviews/StarRating.jsx';
+import calculateRating from '../../helper.js';
+import { getReviewsMeta } from '../../reviewRequest.js';
+
+const RelatedCard = ({ product, currProduct }) => {
+  const [relatedImg, setRelatedImg] = useState('');
+  const [salePrice, setSalePrice] = useState('');
+  const [defaultPrice, setDefaultPrice] = useState('');
+  const [inOutfit, setInOutfit] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [ratings, setRatings] = useState({});
+  const ratingsBreakDown = useMemo(() => calculateRating(ratings), [ratings]);
+
+  const setDefaultData = (stylesObj) => {
+    let relatedStyleInd = 0;
+    for (let i = 0; i < stylesObj.results.length; i++) {
+      if (stylesObj.results[i]['default?']) {
+        relatedStyleInd = i;
+        break;
+      }
+    }
+    setRelatedImg(stylesObj.results[relatedStyleInd].photos[0].thumbnail_url);
+    setDefaultPrice(product.default_price);
+    setSalePrice(stylesObj.results[relatedStyleInd].sale_price);
+  };
+  const getRelatedStylesFromIds = () => {
+    axios.get(`${options.url}products/${product.id}/styles`, {
+      headers: options.headers,
+    })
+      .then((res) => {
+        setDefaultData(res.data);
+      })
+      .catch((res, err) => {
+        res.end('Could not get related styles: ', err);
+      });
+  };
+
+  useEffect(() => {
+    getRelatedStylesFromIds();
+    getReviewsMeta(product.id)
+      .then((res) => {
+        setRatings(res.ratings);
+      });
+  }, []);
+
+  const handleStarClick = () => {
+    const newInOutfit = !inOutfit;
+    setInOutfit(newInOutfit);
+  };
+
+  return (
+    <div className="list-card">
+      <img
+        src={relatedImg}
+        alt={product.name}
+        width="200"
+        height="200"
+        className="card-img"
+      />
+      <div className="card-add-star" onClick={() => (handleStarClick())}>
+        {inOutfit ? <img src={fillStar} alt="star_icon_fill" />
+          : <img src={emptyStar} alt="star_icon_empty" />}
+      </div>
+      <div className="card-category">
+        {product.category.toUpperCase()}
+      </div>
+      <div className="card-name">
+        {product.name}
+      </div>
+      <div className="card-price">
+        <div className="card-default-price">
+          $
+          {defaultPrice}
+        </div>
+        <div className="card-sale-price">{salePrice || ''}</div>
+      </div>
+      <div className="card-rating">
+        {Number.isNaN(ratingsBreakDown.averageRatings.toFixed(1))
+          ? <div className="card-rating-num">No ratings</div>
+          : (
+            <>
+              <div className="card-rating-num">{ratingsBreakDown.averageRatings.toFixed(1)}</div>
+              <StarRating rating={ratingsBreakDown.averageRatings} />
+            </>
+          )}
+      </div>
+      <div className="modal-comparison">
+        <button type="button" className="btn-modal-comparison" onClick={() => (setIsOpen(true))}>Compare</button>
+        <ComparisonModal
+          key={`comp${product.id}`}
+          open={isOpen}
+          product={product}
+          currProduct={currProduct}
+          onClose={() => (setIsOpen(false))}
+        />
+      </div>
+    </div>
+  );
+};
+
+RelatedCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    category: PropTypes.string,
+    default_price: PropTypes.string,
+    name: PropTypes.string,
+    features: PropTypes.instanceOf(Array),
+  }),
+  currProduct: PropTypes.shape({
+    id: PropTypes.number,
+    category: PropTypes.string,
+    default_price: PropTypes.string,
+    name: PropTypes.string,
+    features: PropTypes.instanceOf(Array),
+
+  }),
+};
+
+RelatedCard.defaultProps = {
+  product: {},
+  currProduct: {},
+};
+
+export default RelatedCard;
---
+++ client/src/RelatedAndOutfit/OutfitList.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+import RelatedCard from './RelatedCard.jsx';
+import './related-list.css';
+import options from '../config/config.js';
+
+const RelatedList = () => {
+  const [currProduct, setCurrProduct] = useState({});
+  const [current, setCurrent] = useState(0);
+  const [len, setLen] = useState(0);
+  const [related, setRelated] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [cards, setCards] = useState(3);
+
+  const getRelatedFromIds = (idList) => {
+    const relatedList = [];
+    idList.forEach((id) => {
+      axios.get(`${options.url}products/${id}`, {
+        headers: options.headers,
+      })
+        .then((res) => {
+          if (id !== 13029) {
+            relatedList.push(res.data);
+            const newRelated = related.concat(relatedList);
+            setRelated(newRelated);
+          } else {
+            setCurrProduct(res.data);
+          }
+        })
+        .catch((res, err) => {
+          res.end('Could not get related from ids: ', err);
+        });
+    });
+  };
+
+  const getRelated = () => {
+    axios.get(`${options.url}products/13029/related`, {
+      headers: options.headers,
+    })
+      .then((res) => {
+        setLen(res.data.length - 1);
+        getRelatedFromIds(res.data);
+      })
+      .catch((res, err) => {
+        res.end('Could not get related: ', err);
+      });
+  };
+
+  const updateWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    getRelated();
+    window.addEventListener('resize', updateWidth);
+    return () => { window.removeEventListener('resize', updateWidth); };
+  }, []);
+
+  useEffect(() => {
+    const possibleCards = Math.floor((windowWidth - 100) / 230);
+    setCards(possibleCards);
+  }, [windowWidth]);
+
+  const listRef = useRef(null);
+
+  const nextCard = () => {
+    let newCurrent = current;
+    if (newCurrent !== len - 1) {
+      newCurrent = current + 1;
+    }
+    if (listRef.current) {
+      listRef.current.scrollBy({
+        top: 0,
+        left: 231,
+        behavior: 'smooth',
+      });
+    }
+    setCurrent(newCurrent);
+  };
+
+  const prevCard = () => {
+    let newCurrent = 0;
+    if (current > 0) {
+      newCurrent = current - 1;
+    }
+    if (listRef.current) {
+      listRef.current.scrollBy({
+        top: 0,
+        left: -231,
+        behavior: 'smooth',
+      });
+    }
+    setCurrent(newCurrent);
+  };
+
+  return (
+    <div className="related">
+      {current !== 0 && <button type="button" className="btn-related-left" onClick={prevCard}>prev</button>}
+      <div className="related-list" style={{ width: `${cards * 230}px` }} ref={listRef}>
+        {related.map((product) => (
+          <RelatedCard key={product.id} product={product} currProduct={currProduct} />
+        ))}
+      </div>
+      {current !== len - cards && <button type="button" className="btn-related-right" onClick={nextCard}>next</button>}
+    </div>
+  );
+};
+
+export default RelatedList;
