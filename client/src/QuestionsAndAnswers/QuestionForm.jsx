5335f13d842dbd72aa7c34dce695f78601d9f1cd
rachel <jones.rachel.a464@gmail.com>
2021-07-30 12:57:32 -0700

    work on question submit function, needs validation

--- client/src/QuestionsAndAnswers/QuestionForm.jsx
+++ client/src/QuestionsAndAnswers/QuestionForm.jsx
@@ -1,26 +1,44 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './QForm.css';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import options from '../config/config.js';
 
 const QuestionForm = ({ showQuestionForm, handleQuestionForm }) => {
   if (!showQuestionForm) return null;
+
+  const [body, setBody] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmitQuestion = () => {
+    
+    axios.post(`${options.url}qa/questions`,
+      {
+        body, name, email, product_id: 13027,
+      }, { headers: options.headers })
+      .then()
+      .catch((err) => {
+        Promise.reject(err);
+      });
+  };
+
   return ReactDOM.createPortal(
-    <>
-      <form>
-        <button type="button" onClick={handleQuestionForm}>X</button>
-        <h3>Ask Your Question</h3>
-        <h4>About the product name</h4>
-        <textarea type="text" maxLength="1000" placeholder="Your Question" required />
+    <div className="question-overlay">
+      <form className="question-form">
+        <button className="XButton" type="button" onClick={handleQuestionForm}>X</button>
+        <h3 className="QTitle">Ask Your Question</h3>
+        <h4 className="QSubTitle">About the product name</h4>
+        <textarea className="QBody" type="text" maxLength="1000" placeholder="Your Question" onChange={(e) => { setBody(e.target.value); }} required />
         <span>What is your nickname?</span>
-        <input type="text" maxLength="1000" placeholder="Example: jackson11!" required />
+        <input className="QName" type="text" maxLength="1000" placeholder="Example: jackson11!" onChange={(e) => { setName(e.target.value); }} required />
         <span>For privacy reasons, do not use your full name or email address.</span>
-        <input type="email" required />
+        <input className="QEmail" type="email" onChange={(e) => { setEmail(e.target.value); }} required />
         <span>For authentication reasons, you will not be emailed.</span>
-        <button type="submit">Submit Question</button>
+        <button className="QSubmit" type="submit">Submit Question</button>
       </form>
-    </>,
+    </div>,
     document.getElementById('questions'),
   );
 };
