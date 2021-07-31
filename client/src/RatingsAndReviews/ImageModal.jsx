d54ef2ec9627f54cc905f19ad42b827bde31c72b
lipingh <lipingh@andrew.cmu.edu>
2021-07-30 17:14:28 -0700

    fix the bug: click one photo only pop one photo modal window

--- client/src/RatingsAndReviews/ImageModal.jsx
+++ client/src/RatingsAndReviews/ImageModal.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const ImageModal = ({ showFullImage, url, onClose }) => {
+const ImageModal = ({ showFullImage, url, id, handleClickPhoto }) => {
   if (!showFullImage) {
     return null;
   }
   return ReactDOM.createPortal(
     <>
-      <button type="button" onClick={onClose}>X</button>
+      <button type="button" onClick={() => handleClickPhoto(id)}>X</button>
       <img src={url} alt="" />
     </>,
     document.getElementById('image-modal'),
