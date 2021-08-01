82c6544170833afa589bc5a6cc7d8ed48f876cea
rachel <jones.rachel.a464@gmail.com>
2021-07-31 20:18:52 -0700

    continue working on useAllQuestions file for scroll

--- client/src/QuestionsAndAnswers/useAllQuestions.js
+++ client/src/QuestionsAndAnswers/useAllQuestions.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import options from '../config/config';
 
-const useAllQuestions = (productId, page, sortOption) => {
+const useAllQuestions = (productId, page, searched) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questionsInfinate, setQuestionsI] = useState([]);
   const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
     setQuestions([]);
-  }, [sortOption, productId]);
+  }, [productId]);
 
   useEffect(() => {
     const { CancelToken } = axios;
@@ -25,7 +25,11 @@ const useAllQuestions = (productId, page, sortOption) => {
         page,
         count: 4,
       },
-    });
+      cancelToken: new CancelToken((c) => { cancel = c; }),
+    })
+    .then((res) => {
+      
+    })
   });
 };
 
