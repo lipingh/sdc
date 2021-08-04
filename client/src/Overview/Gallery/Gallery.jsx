1a531271d3972cec51f0ad0f5993493ac4ca0b0f
Jiaqian Zhu <blairzhu19@gmail.com>
2021-08-03 18:08:33 -0700

    update zoom in feature a bit

--- client/src/Overview/Gallery/Gallery.jsx
+++ client/src/Overview/Gallery/Gallery.jsx
@@ -75,8 +75,8 @@ const Gallery = () => {
       const w = magnifyArea.offsetWidth;
       const h = magnifyArea.offsetHeight;
 
-      clientX = (clientX / w) * 80;
-      clientY = (clientY / h) * 80;
+      clientX = (clientX / w) * 70;
+      clientY = (clientY / h) * 70;
       magnifyImg.style.transform = `translate(-${clientX}%, -${clientY}%) scale(2.5)`;
     }
     magnifyArea.addEventListener('mousemove', listener);
