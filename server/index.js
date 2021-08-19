9264fc250509478fd9eada8ef7ab15ae6b8da3cc
Ubuntu <ubuntu@ip-172-31-45-119.us-east-2.compute.internal>
2021-08-19 22:28:23 +0000

    send token

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(
 );
 app.use(express.static('client/dist'));
 app.get('/loaderio-e5022006837028672ad26d571f464ca2',(req, res)=>{
-	res.send('loaderio-e0d9e09648c9188939e93c8ebc615540');
+	res.send('loaderio-e5022006837028672ad26d571f464ca2');
 })
 app.get('/reviews/', controller.getReviews);
 app.get('/reviews/meta', controller.getReviewsMeta);
